225115eedb6064d04b81811e582a56091ac4dca5
nguyenhuongtra <tra.nh.1905@aptechlearning.edu.vn>
2024-03-16 08:38:41 +0700

    update project

---
+++ project3/App_Start/CustomViewEngine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project3
+{
+    public class CustomViewEngine : RazorViewEngine
+    {
+        public CustomViewEngine() : base()
+        {
+            ViewLocationFormats = new string[] {
+                "~/Views/%1/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml",
+            };
+
+            MasterLocationFormats = new string[] {
+                "~/Views/%1/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml",
+                "~/Views/%1",
+                "~/Views",
+            };
+
+            PartialViewLocationFormats = new string[] {
+                "~/Views/%1/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml",
+                "~/Views/%1",
+                "~/Views",
+            };
+        }
+        protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+        {
+            var section = controllerContext.RouteData.Values["section"]?.ToString() ?? string.Empty;
+            return base.CreatePartialView(controllerContext, partialPath.Replace("%1", section));
+        }
+
+        protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+        {
+            var section = controllerContext.RouteData.Values["section"]?.ToString() ?? string.Empty;
+            return base.CreateView(controllerContext, viewPath.Replace("%1", section), masterPath.Replace("%1", section));
+        }
+
+        protected override bool FileExists(ControllerContext controllerContext, string virtualPath)
+        {
+            var section = controllerContext.RouteData.Values["section"]?.ToString() ?? string.Empty;
+            return base.FileExists(controllerContext, virtualPath.Replace("%1", section));
+        }
+    }
+}
---
+++ project3/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace project3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "",
+                defaults: new { section = "User", controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "project3.User.Controllers" }
+            );
+            routes.MapRoute(
+                name: "User",
+                url: "{section}/{controller}/{action}/{id}",
+                defaults: new { section = "User", controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "project3.User.Controllers" }
+            );
+            routes.MapRoute(
+                name: "Admin",
+                url: "{section}/{controller}/{action}/{id}",
+                defaults: new { section = "Admin", controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "project3.Admin.Controllers" }
+            );
+        }
+    }
+}
