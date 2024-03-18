0ceaaad984cb89a35f47e7a7069963db1046b0fb
hieuthuhai <hieu.nt.2226@aptechlearning.edu.vn>
2024-03-18 22:02:11 +0700

    hieuthu2

---
+++ project3/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project3.Models;
+
+namespace project3.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private dbauctionsystemEntities db = new dbauctionsystemEntities();
+
+        public ActionResult Index()
+        {
+            var categories = db.Categories.Include(c => c.Category2);
+            return View(categories.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Parent_ID = new SelectList(db.Categories, "cat_ID", "NameCat");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cat_ID,NameCat,Parent_ID,Characteristic,Status")] Category category, HttpPostedFileBase Image)
+        {
+            if (!IsDupplicated(category))
+            {
+                if (ModelState.IsValid)
+                {
+                    if (Image != null && Image.ContentLength > 0)
+                    {
+                        if (!Path.GetExtension(Image.FileName).ToLower().Equals(".png") && !Path.GetExtension(Image.FileName).ToLower().Equals(".jpg"))
+                        {
+                            ModelState.AddModelError("Image", "Please choose file type .png or .jpg");
+                            return View(category);
+                        }
+                        if (Image.ContentLength > 300000)
+                        {
+                            ModelState.AddModelError("Image", "This File is out of 300KB");
+                            return View(category);
+                        }
+                        try
+                        {
+                            using (var binaryReader = new BinaryReader(Image.InputStream))
+                            {
+                                category.Image = binaryReader.ReadBytes(Image.ContentLength);
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                            throw;
+                        }
+                    }
+                    category.Status = 1;
+                    db.Categories.Add(category);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ViewBag.Parent_ID = new SelectList(db.Categories, "cat_ID", "NameCat", category.Parent_ID);
+            return View(category);
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Parent_ID = new SelectList(db.Categories, "cat_ID", "NameCat", category.Parent_ID);
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cat_ID,NameCat,Parent_ID,Characteristic")] Category category, HttpPostedFileBase Image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Image != null && Image.ContentLength > 0)
+                {
+                    if (!Path.GetExtension(Image.FileName).ToLower().Equals(".png") && !Path.GetExtension(Image.FileName).ToLower().Equals(".jpg"))
+                    {
+                        ModelState.AddModelError("Image", "Please choose file type .png or .jpg");
+                        return View(category);
+                    }
+                    if (Image.ContentLength > 300000)
+                    {
+                        ModelState.AddModelError("Image", "This File is out of 300KB");
+                        return View(category);
+                    }
+                    try
+                    {
+                        using (var binaryReader = new BinaryReader(Image.InputStream))
+                        {
+                            category.Image = binaryReader.ReadBytes(Image.ContentLength);
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+                category.Status = 1;
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Parent_ID = new SelectList(db.Categories, "cat_ID", "NameCat", category.Parent_ID);
+            return View(category);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            category.Status = 0;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public bool IsDupplicated(Category cat)
+        {
+            bool Flag = false;
+            var Cat = db.Categories.ToList().Where(c => c.NameCat.Equals(cat.NameCat, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            if (Cat != null)
+            {
+                ModelState.AddModelError(nameof(cat.NameCat), "Name is Dupplicate");
+                Flag = true;
+            }
+
+            return Flag;
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ project3/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project3.Models;
+
+namespace project3.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private dbauctionsystemEntities db = new dbauctionsystemEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Customer).Include(p => p.Status).Where(p=>p.Status.sta_ID == 1);
+            ViewBag.Listpro = db.Products.Include(p => p.Customer).Include(p => p.Status).Where(p => p.Status.sta_ID != 1);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ListAu = db.Auctions.Where(a=>a.StartTime>= DateTime.UtcNow).ToList();
+            return View(product);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Inspect(int? id)
+        {
+            Product product = db.Products.Find(id);
+            product.sta_ID = 2;
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
